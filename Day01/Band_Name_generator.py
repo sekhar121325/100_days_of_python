82553677fed144939f455723169022edae470526
sekhar121325 <sekhartanikella1213@gmail.com>
2025-12-30 00:30:11 +0530

    Day 1 Project - Band_Name_generator

---
+++ Day01/Band_Name_generator.py
@@ -0,0 +1,5 @@
+# Day 1 project : Band Name Generator
+print("Hey There! This Is Your Band Name Generator..")
+city = input("Enter The City You Grew Up:\n")
+pet = input("Enter The Name Of Your Pet:\n")
+print("Your Band Name Could Be:\n " + city + " "+pet)
